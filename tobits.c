dca22ef995bd3e1558d535bac5a33fba0d32afaa
LesenHuang <wongrosen@icloud.com>
2023-09-21 09:27:56 +0800

    Significantly added shell input and output.

--- tobits.c
+++ tobits.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 char *toBits(void *value, char bytes)
 {
@@ -44,12 +45,33 @@ char *toBits(void *value, char bytes)
 
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+	char *ch;
 
-    char *ch = toBits(&(char){4}, sizeof(char));
 
-    printf("%s %u\n", ch, 4);
+	if (! --argc) {
+	
+		puts("Usage:\n");
+		printf("\t %s 2 or more int number .eg 09\n", *argv);
+		printf("\t %s only 1 char symbol .eg 'c'\n", *argv);
+		printf("\t %s Need to be spaced by dots. float number .eg 3.14\n", *argv);
+		exit(1);
+	}
+
+	if (strchr(argv[1], '.') != NULL)
+
+		ch = toBits(&(float){atoi(argv[1])}, sizeof(float));
+
+	else if(strlen(argv[1]) == 1)
+
+		ch = toBits(&(char){*argv[1]}, sizeof(char));
+
+	else
+
+		ch = toBits(&(int){atoi(argv[1])}, sizeof(int));
+
+    printf("%s %s\n", ch, argv[1]);
 
     return 0;
 }
